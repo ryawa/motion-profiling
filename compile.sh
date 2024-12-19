323853c5b3bab8172541ba021db3a7bae423c11c
ryawa <22625346+ryawa@users.noreply.github.com>
2024-12-19 16:01:32 -0500

    Use -O3 flag in Makefile

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ src/test.cpp src/Constraints.cpp src/Path.cpp src/Trajectory.cpp -I./include -std=c++20 -O3 -ffast-math -DNDEBUG -march=native -o test
+g++ src/test.cpp src/Constraints.cpp src/Path.cpp src/Trajectory.cpp -I./include -std=c++20 -O3 -o test
